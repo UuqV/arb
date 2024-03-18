b877a473cfd70bd9cff7ffd4ab61af248790caad
Kathryn Lovell <krlovell017@gmail.com>
2024-03-18 15:38:30 -0400

    rename

---
+++ arbot/src/trade.rs
@@ -0,0 +1,58 @@
+use std::env;
+
+use jupiter_swap_api_client::{
+    quote::QuoteRequest, swap::SwapRequest, transaction_config::TransactionConfig,
+    JupiterSwapApiClient,
+};
+use solana_client::nonblocking::rpc_client::RpcClient;
+use solana_sdk::{pubkey, transaction::VersionedTransaction};
+use solana_sdk::{pubkey::Pubkey, signature::NullSigner};
+use tokio;
+
+const USDC_MINT: Pubkey = pubkey!("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
+const NATIVE_MINT: Pubkey = pubkey!("So11111111111111111111111111111111111111112");
+
+pub const TEST_WALLET: Pubkey = pubkey!("2AQdpHJ2JpcEgPiATUXjQxA8QmafFegfQwSLWSprPicm"); // Coinbase 2 wallet
+
+async fn swap(quote_response) {
+    // POST /swap
+    let swap_response = jupiter_swap_api_client
+        .swap(&SwapRequest {
+            user_public_key: TEST_WALLET,
+            quote_response: quote_response.clone(),
+            config: TransactionConfig::default(),
+        })
+        .await
+        .unwrap();
+
+    println!("Raw tx len: {}", swap_response.swap_transaction.len());
+
+    let versioned_transaction: VersionedTransaction =
+        bincode::deserialize(&swap_response.swap_transaction).unwrap();
+
+    // Replace with a keypair or other struct implementing signer
+    let null_signer = NullSigner::new(&TEST_WALLET);
+    let signed_versioned_transaction =
+        VersionedTransaction::try_new(versioned_transaction.message, &[&null_signer]).unwrap();
+
+    // send with rpc client...
+    let rpc_client = RpcClient::new("https://api.mainnet-beta.solana.com".into());
+
+    // This will fail with "Transaction signature verification failure" as we did not really sign
+    let error = rpc_client
+        .send_and_confirm_transaction(&signed_versioned_transaction)
+        .await
+        .unwrap_err();
+    println!("{error}");
+
+    // POST /swap-instructions
+    let swap_instructions = jupiter_swap_api_client
+        .swap_instructions(&SwapRequest {
+            user_public_key: TEST_WALLET,
+            quote_response,
+            config: TransactionConfig::default(),
+        })
+        .await
+        .unwrap();
+    println!("swap_instructions: {swap_instructions:?}");
+}
