f2a8158db105af7d5bff51b219894ed28b86aea0
Kathryn Lovell <krlovell017@gmail.com>
2024-03-19 12:59:16 -0400

    add double quote

--- src/logic.rs
+++ src/logic.rs
@@ -1,9 +1,9 @@
 pub fn should_buy(hist: f64, funding: f64, price: f64) -> bool {
-    return hist < -(funding / 10000.0) && funding > price;
+    return hist < -0.01 && funding > price;
 }
 
-pub fn should_sell(hist: f64, funding: f64, buys: u64) -> bool {
-    return hist > (funding / 10000.0) && buys > 0;
+pub fn should_sell(hist: f64, funding: f64, buys: f64) -> bool {
+    return hist > 0.01 && buys > 1.0;
 }
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,28 @@
 use std::env;
 
-use solana_program::{
-    account_info::AccountInfo,
-    entrypoint,
-    entrypoint::ProgramResult,
-    pubkey::Pubkey,
-    msg,
-};
-
 use jupiter_swap_api_client::{
     quote::QuoteRequest,
     JupiterSwapApiClient,
 };
 use solana_sdk::pubkey;
+use solana_sdk::pubkey::Pubkey;
 use ta::indicators::MovingAverageConvergenceDivergence as Macd;
 use ta::Next;
 use std::thread;
 use std::time::Duration;
+use futures::try_join;
 use tokio;
 
 mod logic;
-mod trade;
 
 const USDC_MINT: Pubkey = pubkey!("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
+const USDC_DECIMALS: f64 = 0.000001;
+
 const NATIVE_MINT: Pubkey = pubkey!("So11111111111111111111111111111111111111112");
+const NATIVE_DECIMALS: f64 = 0.000000001;
+
+pub const TEST_WALLET: Pubkey = pubkey!("2AQdpHJ2JpcEgPiATUXjQxA8QmafFegfQwSLWSprPicm"); // Coinbase 2 wallet
+
 
 
 #[tokio::main]
@@ -38,9 +37,10 @@ async fn macd() {
     let jupiter_swap_api_client = JupiterSwapApiClient::new(api_base_url);
 
     let mut macd = Macd::new(12, 26, 9).unwrap();
-    
-    let quote_request = QuoteRequest {
-        amount: 100,
+
+        
+    let sell_request = QuoteRequest {
+        amount: 1000000000,
         input_mint: NATIVE_MINT,
         output_mint: USDC_MINT,
         slippage_bps: 50,
@@ -48,22 +48,59 @@ async fn macd() {
     };
 
     let initial_funding: f64 = 1000.0;
-    let mut funding : f64 = initial_funding;
+    let mut usdc : f64 = initial_funding;
     let mut profit: f64 = 0.0;
-    let mut buys : u64 = 0;
+    let mut sol : f64 = 0.0;
     println!("Initial funding: {initial_funding:#?}");
     println!("Algorithm: Solid Buy");
-    println!("Price, Histogram, Buys, Funding, Profit");
+    println!("Sell, Histogram, SOL, USDC, Profit");
 
     // GET /quote
-    match jupiter_swap_api_client.quote(&quote_request).await {
-        Ok(quote_response) => {
-            println!("Quote response: {quote_response:#?}");
+    loop {
+        match jupiter_swap_api_client.quote(&sell_request).await {
+            Ok(sell_response) => {
 
-            trade::swap(quote_response, jupiter_swap_api_client).await;
-        },
-        Err(_e) => {
-            println!("Quote error: {_e:#?}");
+
+                let price = sell_response.out_amount as f64 * USDC_DECIMALS;
+                let next = macd.next(price);
+                let hist = next.histogram;
+
+                if logic::should_sell(hist, usdc, sol) {
+                    usdc = usdc + price;
+                    if usdc > initial_funding {
+                        profit = profit + (usdc - initial_funding);
+                        usdc = initial_funding;
+                    }
+                    sol = sol - 1.0;
+                }
+
+                let buy_request = QuoteRequest {
+                    amount: sell_response.out_amount,
+                    input_mint: USDC_MINT,
+                    output_mint: NATIVE_MINT,
+                    slippage_bps: 50,
+                    ..QuoteRequest::default()
+                };
+
+                match jupiter_swap_api_client.quote(&buy_request).await {
+                    Ok(buy_response) => {
+                        if logic::should_buy(hist, usdc, price) {
+                            usdc = usdc - price;
+                            sol = sol + buy_response.out_amount as f64 * NATIVE_DECIMALS;
+                        }
+                        println!("{price:#?}, {hist:#?}, {sol:#?}, {usdc:#?}, {profit:#?}");
+                    },
+                    Err(_e) => {
+                        thread::sleep(Duration::from_secs(2));
+
+                    }
+                }
+
+                thread::sleep(Duration::from_secs(2));
+            },
+            Err(_) => {
+                thread::sleep(Duration::from_secs(2));
+            }
         }
     }
 
