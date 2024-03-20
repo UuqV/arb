d04b38b0ed6f9a8a6991a7011cfed5634ed2b813
Kathryn Lovell <krlovell017@gmail.com>
2024-03-20 08:59:47 -0400

    tests

--- src/logic.rs
+++ src/logic.rs
@@ -1,5 +1,5 @@
 pub fn should_buy(hist: f64, funding: f64, price: f64, hist_threshold: f64) -> bool {
-    return hist < -(hist_threshold) && funding > price;
+    return hist < -(hist_threshold) && funding > price + 10.0;
 }
 
 pub fn should_sell(hist: f64, sol: f64, price: f64, hist_threshold: f64) -> bool {
@@ -13,31 +13,36 @@ mod buy_tests {
 
     #[test]
     fn buy_with_funding() {
-        assert_eq!(should_buy(-1.0, 1000.0, 100.0, .01), true);
+        assert_eq!(should_buy(-1.0, 1000.0, 100.0, 0.01), true);
     }
 
     #[test]
     fn buy_but_no_funding() {
-        assert_eq!(should_buy(-1.0, 1.0, 100.0, .01), false);
+        assert_eq!(should_buy(-1.0, 1.0, 100.0, 0.01), false);
+    }
+
+    #[test]
+    fn buy_but_not_quite_funding() {
+        assert_eq!(should_buy(-1.0, 110.0, 100.0, 0.01), false);
     }
 
     #[test]
     fn negative_no_buy_with_funding() {
-        assert_eq!(should_buy(-0.0000001, 1000.0, 100.0, .01), false);
+        assert_eq!(should_buy(-0.0000001, 1000.0, 100.0, 0.01), false);
     }
 
     #[test]
     fn positive_no_buy_with_funding() {
-        assert_eq!(should_buy(9.0, 1000.0, 100.0, .01), false);
+        assert_eq!(should_buy(9.0, 1000.0, 100.0, 0.01), false);
     }
 
     #[test]
     fn positive_no_buy_no_funding() {
-        assert_eq!(should_buy(9.0, 1.0, 100.0, .01), false);
+        assert_eq!(should_buy(9.0, 1.0, 100.0, 0.01), false);
     }
 
     #[test]
     fn negative_no_buy_no_funding() {
-        assert_eq!(should_buy(-0.0000001, 10.0, 100.0, .01), false);
+        assert_eq!(should_buy(-0.0000001, 10.0, 100.0, 0.01), false);
     }
 }
